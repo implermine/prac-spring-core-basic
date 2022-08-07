e0efa9e7be19061a4abb97a0c5499b551cb8b04b
이태강 <lkorea12345@gmail.com>
2022-08-07 21:07:58 +0900

    feat: DI 컨테이너 관련 docs 추가

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ref
+
+---
+[김영한 : 스프링-핵심-원리-기본편](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/dashboard)
+
+
+# Miscellaneous
+
+---
+
+## 프레임워크 vs 라이브러리
+> 프레임워크가 내가 작성한 코드를 제어하고, 대신 실행하면 그것은 프레임워크가 맞다. (JUnit)
+반면에 내가 작성한 코드가 직접 제어의 흐름을 담당한다면 그것은 프레임워크가 아니라 라이브러리다.
+
--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -14,6 +14,12 @@ import org.example.order.OrderServiceImpl;
  * DIP 와 OCP를 지키기 위해, '누군가'가 인터페이스에 구현체를 주입해줘야 한다.
  *
  * 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스를 만들자.
+ *
+ *
+ * ###
+ * AppConfig처럼 객체를 생성하고 관리하면서 의존관계를 연결해 주는 것을
+ * 'IOC 컨테이너' 또는 'DI 컨테이너'라 한다.
+ * ###
  */
 public class AppConfig {
 
@@ -22,11 +28,19 @@ public class AppConfig {
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(this.memberRepository(),this.fixDiscountPolicy());
+        return new OrderServiceImpl(this.memberRepository(),this.discountPolicy());
     }
 
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
+    }
+
+    /**
+     * fix로 결정
+     */
+    public DiscountPolicy discountPolicy(){
+        return this.fixDiscountPolicy();
+//        return this.rateDiscountPolicy();
     }
 
     public DiscountPolicy fixDiscountPolicy(){
