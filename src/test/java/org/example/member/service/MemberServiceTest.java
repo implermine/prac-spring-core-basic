4cc38872c4c0b294f6e8640117a81cfa8bcab4f5
이태강 <lkorea12345@gmail.com>
2022-08-07 19:43:37 +0900

    test: 테스트코드 DisplayName 추가, ShouldNamingConvention 변경

--- src/test/java/org/example/member/service/MemberServiceTest.java
+++ src/test/java/org/example/member/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.example.member.Grade;
 import org.example.member.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,17 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    void join() {
+    @DisplayName("회원가입")
+    void should_join_member() {
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
@@ -31,10 +24,6 @@ class MemberServiceTest {
         Member foundMember = memberService.findMember(1L);
 
         //then
-        Assertions.assertThat(member).isEqualTo(member);
-    }
-
-    @Test
-    void findMember() {
+        Assertions.assertThat(foundMember).isEqualTo(member);
     }
 }
--- src/test/java/org/example/order/OrderServiceIntegrationTest.java
+++ src/test/java/org/example/order/OrderServiceIntegrationTest.java
@@ -5,17 +5,17 @@ import org.example.member.Grade;
 import org.example.member.Member;
 import org.example.member.service.MemberService;
 import org.example.member.service.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class OrderServiceTest {
+class OrderServiceIntegrationTest {
 
     MemberService memberService = new MemberServiceImpl();
     OrderService orderService = new OrderServiceImpl();
 
     @Test
-    void createOrder(){
+    @DisplayName("주문 생성과 그에따른 할인 로직 테스트")
+    void should_create_order_via_proper_discount_logic(){
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
